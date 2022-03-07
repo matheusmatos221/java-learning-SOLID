781797453c365e5b093b53a52ac2287ce333cbc2
Matheus Matos <matheusmatos.221@hotmail.com>
2022-03-07 14:45:10 -0300

    Extraindo validações

--- src/br/com/alura/rh/service/ReajusteService.java
+++ src/br/com/alura/rh/service/ReajusteService.java
@@ -7,24 +7,21 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
 
 public class ReajusteService {
+
+    private List<ValidacaoReajuste> validacoes;
+
+    public ReajusteService(List<ValidacaoReajuste> validacoes){
+        this.validacoes = validacoes;
+    }
+
     public void reajustarSalarioDoFuncionario(Funcionario funcionario, BigDecimal aumento){
+        this.validacoes.forEach(v -> v.validar(funcionario, aumento));
 
-        BigDecimal salarioAtual = funcionario.getSalario();
-        BigDecimal percentualReajuste = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
-        if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
-            throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
-        }
-
-        LocalDate dataUltimoReajuste = funcionario.getDataUltimoReajuste();
-        LocalDate dataAtual = LocalDate.now();
-        long mesesDesdeUltimoReajuste = ChronoUnit.MONTHS.between(dataUltimoReajuste, dataAtual);
-        if(mesesDesdeUltimoReajuste < 6){
-            throw new ValidacaoException("Intervalo entre reajustes deve ser de no mínimo 6 meses!");
-        }
-
-        BigDecimal salarioReajustado = salarioAtual.add(aumento);
+        BigDecimal salarioReajustado = funcionario.getSalario().add(aumento);
         funcionario.atualizarSalario(salarioReajustado);
+
     }
 }

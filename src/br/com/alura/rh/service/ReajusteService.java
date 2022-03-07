f91b9b808b48cc5f0f18822511715372ca63ba91
Matheus Matos <matheusmatos.221@hotmail.com>
2022-03-07 14:33:15 -0300

    ADD new business rule 'peridiocidade'

--- src/br/com/alura/rh/service/ReajusteService.java
+++ src/br/com/alura/rh/service/ReajusteService.java
@@ -6,6 +6,7 @@ import br.com.alura.rh.model.Funcionario;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 public class ReajusteService {
     public void reajustarSalarioDoFuncionario(Funcionario funcionario, BigDecimal aumento){
@@ -15,6 +16,14 @@ public class ReajusteService {
         if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
             throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
         }
+
+        LocalDate dataUltimoReajuste = funcionario.getDataUltimoReajuste();
+        LocalDate dataAtual = LocalDate.now();
+        long mesesDesdeUltimoReajuste = ChronoUnit.MONTHS.between(dataUltimoReajuste, dataAtual);
+        if(mesesDesdeUltimoReajuste < 6){
+            throw new ValidacaoException("Intervalo entre reajustes deve ser de no mínimo 6 meses!");
+        }
+
         BigDecimal salarioReajustado = salarioAtual.add(aumento);
         funcionario.atualizarSalario(salarioReajustado);
     }
